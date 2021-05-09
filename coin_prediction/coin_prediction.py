0c387876877f96d990ef2256fc59974ec51d3192
elon-kim <eunjong147@pusan.ac.kr>
2021-05-10 01:43:51 +0900

    mysql 연결 성공

--- coin_prediction/coin_prediction.py
+++ coin_prediction/coin_prediction.py
@@ -1,8 +1,20 @@
 import pyupbit
 import pandas as pd
+import mplfinance as mpf
+import matplotlib as plt
+import pymysql
 
 tickers = pyupbit.get_tickers(fiat="KRW") # KRW/BTC/ETH/USDT
 print(len(tickers), tickers)
 
-df = pyupbit.get_ohlcv("KRW-BTC", interval="week", count=200).drop('value', axis=1)
+df = pd.DataFrame(pyupbit.get_ohlcv("KRW-BTC", interval="week", count=10).drop('value', axis=1))
 print(df.head())
+
+#mpf.plot(df, type='candle')
+
+connection = pymysql.connect(host='localhost', port=0, db='Coin', user='root', passwd='Rladmswhd@1', autocommit=True)
+cursor = connection.cursor()
+cursor.execute("SELECT VERSION();")
+result = cursor.fetchone()
+print(result)
+connection.close()
