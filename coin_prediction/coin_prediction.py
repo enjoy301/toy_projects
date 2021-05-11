01d10b10e5a1eeae826ba9df97126fc48db25671
elon-kim <eunjong147@pusan.ac.kr>
2021-05-12 00:49:52 +0900

    update_code_info 함수 완성, update_price_week 함수 제작 중

--- coin_prediction/coin_prediction.py
+++ coin_prediction/coin_prediction.py
@@ -3,18 +3,30 @@ import pandas as pd
 import mplfinance as mpf
 import matplotlib as plt
 import pymysql
+from datetime import datetime
 
-tickers = pyupbit.get_tickers(fiat="KRW") # KRW/BTC/ETH/USDT
-print(len(tickers), tickers)
+def update_code_info(cursor):
+    tickers = pyupbit.get_tickers(fiat="KRW")  # KRW/BTC/ETH/USDT
+    cursor.execute("select * from coin_info;")
+    result = cursor.fetchall()
+    code_list = [list(i)[0] for i in result]
+    if sorted(tickers) != code_list:
+        for i in range(len(tickers)):
+            print("coin 목록 UPDATE")
+            sql = f"REPLACE INTO coin_info(code) VALUES('{tickers[i]}');"
+            cursor.execute(sql)
 
-df = pd.DataFrame(pyupbit.get_ohlcv("KRW-BTC", interval="week", count=10).drop('value', axis=1))
-print(df.head())
-
-#mpf.plot(df, type='candle')
+def update_price_week(today):
+    df = pd.DataFrame(pyupbit.get_ohlcv("KRW-BTC", interval="week", count=100).drop('value', axis=1))
+    for i in range(len(df)):
+        print(df.iloc[i])
+    # mpf.plot(df, type='candle')
 
 connection = pymysql.connect(host='localhost', port=0, db='Coin', user='root', passwd='Rladmswhd@1', autocommit=True)
 cursor = connection.cursor()
-cursor.execute("SELECT VERSION();")
-result = cursor.fetchone()
-print(result)
+today = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+
+#update_code_info(cursor)
+#update_price_week(today)
+
 connection.close()
