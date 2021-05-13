8879765964d799446f17191efd42868699a3ee27
elon-kim <eunjong147@pusan.ac.kr>
2021-05-13 22:56:20 +0900

    update_price_week 함수 끝

--- coin_prediction/coin_prediction.py
+++ coin_prediction/coin_prediction.py
@@ -11,22 +11,38 @@ def update_code_info(cursor):
     result = cursor.fetchall()
     code_list = [list(i)[0] for i in result]
     if sorted(tickers) != code_list:
+        print("coin 목록 UPDATE 중...")
         for i in range(len(tickers)):
-            print("coin 목록 UPDATE")
             sql = f"REPLACE INTO coin_info(code) VALUES('{tickers[i]}');"
             cursor.execute(sql)
+        print("coin 목록 UPDATE 완료!")
 
-def update_price_week(today):
-    df = pd.DataFrame(pyupbit.get_ohlcv("KRW-BTC", interval="week", count=100).drop('value', axis=1))
+def update_price_week(code, cursor, today):
+    coin_symbol = code[code.index('-')+1:]
+    print(coin_symbol+"_price_week UPDATE 중...")
+    check_first = 1
+    df = pd.DataFrame(pyupbit.get_ohlcv(code, interval="week", count=1000).drop('value', axis=1))
     for i in range(len(df)):
-        print(df.iloc[i])
-    # mpf.plot(df, type='candle')
+        if check_first != 1:
+            today_close = float(df.iloc[i]['close'])
+            yesterday_close = float(df.iloc[i-1]['close'])
+            diff_rate = (today_close-yesterday_close)*100/yesterday_close
+        else:
+            diff_rate = 0
+            check_first = 0
+        date = datetime.strptime(str(df.index[i]), '%Y-%m-%d %H:%M:%S').strftime('%Y%m%d%H%M%S')
+        sql = f"REPLACE INTO "+coin_symbol+"_price_week(date, open, high, low, close, volumn, diff_rate) " \
+              f"VALUES({date}, {df.iloc[i]['open']}, {df.iloc[i]['high']}, {df.iloc[i]['low']}, " \
+              f"{df.iloc[i]['close']}, {df.iloc[i]['volume']}, {diff_rate});"
+        cursor.execute(sql)
+    print(coin_symbol+"_price_week UPDATE 완료!")
+    #mpf.plot(df, type='candle')
 
 connection = pymysql.connect(host='localhost', port=0, db='Coin', user='root', passwd='Rladmswhd@1', autocommit=True)
 cursor = connection.cursor()
 today = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
 #update_code_info(cursor)
-#update_price_week(today)
+#update_price_week("KRW-BTC", cursor, today)
 
 connection.close()
