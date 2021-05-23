876398782b336409c79d9bf14f19e5bf36e5beb4
elon-kim <eunjong147@pusan.ac.kr>
2021-05-23 19:19:42 +0900

    ADD testcode when there is no post

--- django_blog/blog/tests.py
+++ django_blog/blog/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
 
 # Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(soup.title.text, 'Blog')
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        self.assertEqual(Post.objects.count(), 0)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('아직 게시물이 없습니다', main_area.text)
+
+        post_001 = Post.objects.create(
+            title='첫 번째',
+            content="Hello.",
+        )
+        post_002 = Post.objects.create(
+            title='두 번째',
+            content="두 번째다 왱",
+        )
+        self.assertEqual(Post.objects.count(), 2)
+
+        response = self.client.get('/blog/')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(response.status_code, 200)
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(post_001.title, main_area.text)
+        self.assertNotIn('아직 게시물이 없습니다', main_area.text)
