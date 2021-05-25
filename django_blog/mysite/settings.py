4df84a5cb5934ba2802865f2f2043cb6501cd634
elon-kim <eunjong147@pusan.ac.kr>
2021-05-26 00:01:11 +0900

    ADD gunicorn in docker

--- django_blog/mysite/settings.py
+++ django_blog/mysite/settings.py
@@ -116,6 +116,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
