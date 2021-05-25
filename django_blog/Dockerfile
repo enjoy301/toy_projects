08ebe42d77f5b256a0e87ff02c06fcb2157b4c27
elon-kim <eunjong147@pusan.ac.kr>
2021-05-25 23:33:48 +0900

    ADD docker and CHANGE to postgreSQL

---
+++ django_blog/Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.8.0-alpine
+
+WORKDIR /usr/src/app
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+RUN apk update
+RUN apk add postgresql-dev gcc python3-dev musl-dev zlib-dev jpeg-dev
+
+COPY . /usr/src/app
+
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
--- django_blog/mysite/settings.py
+++ django_blog/mysite/settings.py
@@ -9,12 +9,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fov6-^+32s3k-&04i(04a-)b^yo+7r@boh6dh3zn9b!t=9=5n3'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fov6-^+32s3k-&04i(04a-)b^yo+7r@boh6dh3zn9b!t=9=5n3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
 
-ALLOWED_HOSTS = []
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -66,8 +69,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("SQL_ENGINE", 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get("SQL_PORT", '5432'),
     }
 }
 
