6a9b6686d1afbb024e720c256567d882aa0c800b
elon-kim <eunjong147@pusan.ac.kr>
2021-05-28 01:00:00 +0000

    ADD test function 'test_post_detail'

--- django_blog/blog/tests.py
+++ django_blog/blog/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as BS
 from .models import Post
 
 # Create your tests here.
@@ -10,7 +10,7 @@ class TestView(TestCase):
     def test_post_list(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BS(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
         navbar = soup.nav
         self.assertIn('Blog', navbar.text)
@@ -31,8 +31,31 @@ class TestView(TestCase):
         self.assertEqual(Post.objects.count(), 2)
 
         response = self.client.get('/blog/')
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BS(response.content, 'html.parser')
         self.assertEqual(response.status_code, 200)
         main_area = soup.find('div', id='main-area')
         self.assertIn(post_001.title, main_area.text)
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+
+    def test_post_detail(self):
+        post_001 = Post.objects.create(
+            title = '첫 번째',
+            content = 'Hello World.',
+        )
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1')
+
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BS(response.content, 'html.parser')
+
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        self.assertIn(post_001.title, soup.title.text)
+
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+
+        self.assertIn(post_001.content, post_area.text)
